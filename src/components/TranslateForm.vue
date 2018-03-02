f804c4d78d3b93672aa879efb56fa4a214e26441
shridatta <sgz2@njit.edu>
2018-03-02 00:24:21 -0500

    creating translate form

---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="TranslateForm">
+   <h1> TranslateForm </h1>
+   <form v-on:submit="formSubmit">
+    <input type="text" v-model="textToTranslate" placeholder="enter a text">
+    <input type='submit' value='Translate'>
+   </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TranslateForm',
+  data(){
+    return
+    {
+      textToTranslate: ''
+    }
+  },
+  methods:{
+    formSubmit(e)
+  {
+    alert('submitted');
+    e.preventDefault();
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
