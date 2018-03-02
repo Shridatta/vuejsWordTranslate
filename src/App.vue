280f8faac8180a49521e3e91bb411014d3bbdf71
shridatta <sgz2@njit.edu>
2018-03-02 02:08:58 -0500

    created translatedOutput component

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,30 @@
   <div id="app">
    <h1> Word Translator </h1>
    <TranslateForm v-on:formSubmit="translateText"/>
+   <translatedOutput v-text="translatedText" />
   </div>
 </template>
 
 <script>
 import TranslateForm from './components/TranslateForm'
+import translatedOutput from './components/translatedOutput'
 export default {
   name: 'App',
   components: {
-    TranslateForm
+    TranslateForm,
+    translatedOutput
   },
-  methods:
+  data: function()
   {
+    return{
+      translatedText:''
+    }
+  },
+  methods:{
     translateText: function(text){
       this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180302T031807Z.fb5815557a96c863.1511680e19ff6834c42242f780b8fc6b214f01ed&lang=es&text='+text)
       .then((response)=>{
-        console.log(response);
+      this.translatedText = response.body.text[0];
       });
     }
   }
