ac4fe8050d08e19e4d6e1b35c22c9673c974872f
shridatta <sgz2@njit.edu>
2018-03-02 02:23:49 -0500

    added other languages

--- src/App.vue
+++ src/App.vue
@@ -22,8 +22,8 @@ export default {
     }
   },
   methods:{
-    translateText: function(text){
-      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180302T031807Z.fb5815557a96c863.1511680e19ff6834c42242f780b8fc6b214f01ed&lang=es&text='+text)
+    translateText: function(text,language){
+      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180302T031807Z.fb5815557a96c863.1511680e19ff6834c42242f780b8fc6b214f01ed&lang='+language+'&text='+text)
       .then((response)=>{
       this.translatedText = response.body.text[0];
       });
--- src/components/TranslateForm.vue
+++ src/components/TranslateForm.vue
@@ -3,6 +3,12 @@
    <h1> TranslateForm </h1>
    <form v-on:submit="formSubmit">
     <input type="text" v-model="textToTranslate" placeholder="enter a text">
+    <select v-model="language">
+      <option value='es'>Spanish</option>
+      <option value='ja'>Japanese</option>
+      <option value='hi'>Hindi</option>
+      <option value='mr'>Marathi</option>
+    </select>
     <input type='submit' value='Translate'>
    </form>
   </div>
@@ -12,15 +18,18 @@
 export default {
   name: 'TranslateForm',
   data(){
-    return
-    {
-      textToTranslate: ''
+    return {
+      textToTranslate: '',
+      language: ''
     }
+  },
+  created(){
+    this.language='es';
   },
   methods:{
     formSubmit(e)
   {
-    this.$emit('formSubmit',this.textToTranslate);
+    this.$emit('formSubmit',this.textToTranslate,this.language);
     e.preventDefault();
   }
 }
